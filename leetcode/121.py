4c58036b362dffa1543d2098b3a5792d0a62a881
KimHyeonJin <hyeonjin9819@gmail.com>
2022-10-20 14:46:45 +0900

    fix/leetcode_121
    문제풀이 완료

--- leetcode/121.py
+++ leetcode/121.py
@@ -1,19 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-
-        is_sorted = (sorted(prices)[::-1] == prices)  # 내침차순이면 True, 아니면 False
-        profit = 0
-        if is_sorted == True:
-            print(0)
-            return
-        else:
-            profit = 0
-            small = prices[0]
-            big = prices[0]
-            profit = []
-            print(min(prices))
-            for i in range(len(prices)):
-                minNum = min(prices)
-                maxNum = max(prices)
-                profit.append(maxNum - minNum)
-            print(profit)
+        first = prices[0]
+        answer = 0
+        for i in range(len(prices)):
+            profit = prices[i] - first
+            if profit > answer:
+                answer = profit
+            if first > prices[i]:
+                first = prices[i]
+        return answer
