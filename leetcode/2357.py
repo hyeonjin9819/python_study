7772ff6213037fbba12041364a085086c92b2396
KimHyeonJin <hyeonjin9819@gmail.com>
2022-10-23 19:28:09 +0900

    leetcode 2357 문제 풀이 완료

--- leetcode/2357.py
+++ leetcode/2357.py
@@ -1,25 +1,18 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
         nums.sort()
-        print(nums)
         count = 0
 
-        a = []
-
-        while (1):
-            for i in range(len(nums)):
-                if nums[0] == 0:
-                    if (len(nums) == 0):
-                        nums.remove(0)
-                        return count
-                    else:
-                        nums.remove(0)
-                        count = count + 1
-                        continue
-                else:
-                    print(nums)
-                    small = nums[0]
-                    print(nums[i - 1] - small)
-
-
-
+        while nums[len(nums) - 1] != 0:
+            if 0 in nums:
+                remove_set = {0}
+                nums = [i for i in nums if i not in remove_set]  # 리스트 컴프리헨션
+                minValue = nums[0]
+                for i in range(len(nums)):
+                    nums[i] = nums[i] - minValue
+            else:
+                minValue = nums[0]
+                for i in range(len(nums)):
+                    nums[i] = nums[i] - minValue
+            count += 1
+        return count
