16462fa22434c39d23e303e45a07a663f7f03f5c
KimHyeonJin <hyeonjin9819@gmail.com>
2022-11-16 01:05:15 +0900

    백준 1357 풀이완료

---
+++ baekjoon/1357.py
@@ -0,0 +1,12 @@
+import sys
+
+sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+
+data = list(map(int, input().split()))
+
+first = int(str(data[0])[::-1])
+sec = int(str(data[1])[::-1])
+pre = first + sec
+ans = int(str(pre)[::-1])
+print(ans)
