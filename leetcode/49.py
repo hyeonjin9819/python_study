98fed8be9142ecf19038233f2f83fbeb94bf0f3d
KimHyeonJin <hyeonjin9819@gmail.com>
2022-12-28 00:46:50 +0900

    leetcode 49번 풀이 수정완료

--- leetcode/49.py
+++ leetcode/49.py
@@ -1,15 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = []
-        li = []
-        b = {}
-        for data in strs:
-            li.append(sorted(data))
-        print(li)
-        num = len(li)
-        for item in strs:
-            if item in b:
-                b[item].append(item)
-            else:
-                b[item] = [item]
-        print(list(b.values()))
+        dic = collections.defaultdict(list)
+        for word in strs:
+            dic[''.join(sorted(word))].append(word)
+        return dic.values()
