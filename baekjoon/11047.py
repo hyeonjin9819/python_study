534d49d64cbaa21bdb0ec36179f325cd731da7af
KimHyeonJin <hyeonjin9819@gmail.com>
2023-07-12 12:11:16 +0900

    백준 11047 풀이완료

---
+++ baekjoon/11047.py
@@ -0,0 +1,25 @@
+import sys
+sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+n,m = list(map(int, input().split()))
+
+data = []
+
+for i in range(n):
+    a = int(input())
+    if a <= m:
+        data.append(a)
+
+data = sorted(data, reverse=True)
+
+cnt = 0
+i =0
+
+while m != 0:
+    if m // data[i] >0:
+        cnt += m // data[i]
+        m -= m // data[i] * data[i]
+    else:
+        i += 1
+
+print(cnt)
