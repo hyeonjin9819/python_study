aa6aae05adae4768ccc0e4d8ad2cd641221a9b60
KimHyeonJin <hyeonjin9819@gmail.com>
2023-07-20 15:33:58 +0900

    백준 15655 풀이완료

---
+++ baekjoon/15655.py
@@ -0,0 +1,22 @@
+import sys
+
+sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+
+n, m = map(int, input().split())  # 4, 2
+num = sorted(list(map(int, input().split())))
+
+li = []
+
+def dfs(idx, cnt):
+    if cnt == m:
+        print(*li)
+        return
+
+    for i in range(idx, len(num)):
+        if num[i] not in li:
+            li.append(num[i])
+            dfs(i+1, cnt+1)
+            li.pop()
+dfs(0, 0)
+
